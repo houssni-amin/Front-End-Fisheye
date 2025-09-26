79e4469c15d7847075168dcb9a81f6a9db403e44
Amin <houssni.amin@outlook.fr>
2025-09-26 11:32:23 +0200

    feat(home): finalize homepage

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,33 +1,37 @@
 function photographerTemplate(data) {
-  const { portrait, name, city, country, tagline, price } = data
+  const { portrait, name, city, country, tagline, price, id } = data
 
   const picture = `../assets/Sample Photos/Photographers ID Photos/${portrait}`
 
   function getUserCardDOM() {
     const article = document.createElement("article")
 
+    const photographer = document.createElement("a")
+    photographer.setAttribute("href", `photographer.html?id=${id}`)
+    article.appendChild(photographer)
+
     const img = document.createElement("img")
     img.setAttribute("src", picture)
-    article.appendChild(img)
+    photographer.appendChild(img)
 
     const h2 = document.createElement("h2")
     h2.textContent = name
-    article.appendChild(h2)
+    photographer.appendChild(h2)
 
     const p1 = document.createElement("p")
     p1.className = "photographer-location"
     p1.textContent = `${city}, ${country}`
-    article.appendChild(p1)
+    photographer.appendChild(p1)
 
     const p2 = document.createElement("p")
     p2.className = "photographer-tagline"
     p2.textContent = tagline
-    article.appendChild(p2)
+    photographer.appendChild(p2)
 
     const p3 = document.createElement("p")
     p3.className = "photographer-price"
     p3.textContent = `${price}€/jour`
-    article.appendChild(p3)
+    photographer.appendChild(p3)
     return article
   }
   return { name, picture, getUserCardDOM }
