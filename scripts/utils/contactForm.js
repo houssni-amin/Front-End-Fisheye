3a418f3c4a39c822959b2c937a9e27e7e69fa085
Amin <houssni.amin@outlook.fr>
2025-11-06 13:31:55 +0100

    feat: finalize form submission

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -7,3 +7,27 @@ function closeModal() {
   const modal = document.getElementById("contact-modal")
   modal.style.display = "none"
 }
+
+const contactForm = document.querySelector("#contact-modal form")
+
+if (contactForm) {
+  contactForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const prenom = document.getElementById("prenom").value
+    const nom = document.getElementById("nom").value
+    const email = document.getElementById("email").value
+    const message = document.getElementById("message").value
+
+    console.log("--- Données du Formulaire ---")
+    console.log("Prénom:", prenom)
+    console.log("Nom:", nom)
+    console.log("Email:", email)
+    console.log("Message:", message)
+    console.log("-----------------------------")
+
+    closeModal()
+
+    alert(`Merci ${prenom} ! Votre message a été envoyé.`)
+  })
+}
